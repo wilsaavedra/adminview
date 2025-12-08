8d29269489cc0b8a005fed5a61ab0c29b0b2b652
wilsaavedra <wilson.saavedra@outlook.com>
2025-12-08 13:45:04 -0400

    tabla resp5

--- src/App.tsx
+++ src/App.tsx
@@ -79,17 +79,17 @@ function AppContent() {
       {showSidebar && <Sidebar />}
 
     <Fade in timeout={400}>
-  <Box
-    sx={{
-      flexGrow: 1,
-      minWidth: 0,                 // 🔥 ESTO ARREGLA TODO EL COMPORTAMIENTO DE TABLAS
-      ml: showSidebar ? { md: "260px" } : 0,
-      width: "100%",
-      boxSizing: "border-box",
-      p: { xs: 2, sm: 3, md: 4 },
-      overflowX: "auto",          // único scroll global
-    }}
-  >
+ <Box
+  sx={{
+    flexGrow: 1,
+    minWidth: "100%",      // ← ← ← 🔥 SOLUCIÓN REAL
+    ml: showSidebar ? { md: "260px" } : 0,
+    width: "100%",
+    boxSizing: "border-box",
+    p: { xs: 2, sm: 3, md: 4 },
+    overflowX: "auto",
+  }}
+>
           <Routes>
             <Route
               path="/LoginScreen"
